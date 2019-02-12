d9dfc69eb35dcb806203fd3792dfb754efcda8a6
waltzfordebby <lslestercayabyab@gmail.com>
2019-02-12 13:26:37 +0800

    Package Structure and Login-Auth

---
+++ flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dcccd59eb28d67dadba1a144aa390b68'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from flaskblog import routes
